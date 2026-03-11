650e842a29d808acf6225ac8224c1609fbf27c79
apple <apple@appledeMacBook-Pro.local>
2026-03-11 15:27:06 +0800

    update modified infection.py

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,10 +1,10 @@
-n=91 #the total number of students
-m=int(input('The total number of students is 91. Please input the initial number of infected students:')) #the initial number of infected students
-s=float(input('Please input the growing rate of infection:')) #The growing rate of infection
-d=0 #day number
-#calculate the nomber of infected students each day
+n=91 #the total number of students is 91
+m=int(input('The total number of students is 91. Please input the initial number of infected students:')) #get the initial number of infected students
+s=float(input('Please input the growing rate of infection:')) #get the growing rate of infection
+d=0 #initialise day number
+#calculate the number of infected students each day
 while m<n:
-    m=m+m*s
+    m=m+m*s #add newly infected students to the total infected students
     d+=1
     print('The number of infected students on day',d,'is',m)
 print('It takes',d,'days for all students to be infected.')
