9a1562284f55e2d00c6febca4f432396ce966726
apple <apple@appledeMacBook-Pro.local>
2026-03-11 13:14:39 +0800

    Add variables.py

---
+++ Practical4/variables.py
@@ -0,0 +1,14 @@
+#store the variables
+a=5.08
+b=5.33
+c=5.55
+#calculate the differences between 2004 and 2014
+d=b-a
+#calculate the differences between 2014 and 2024
+e=c-b
+#compare the differences
+if d>e:
+    print('The population growth decelerates.')
+else:
+    print('The population growth accelerates.')
+    
