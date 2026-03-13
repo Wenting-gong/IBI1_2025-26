c9d5975b629c9bceacc9fa3b9e402a417b8d0998
apple <apple@appledeMacBook-Pro.local>
2026-03-13 11:04:59 +0800

    update creatine_clearance.py

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -1,33 +1,26 @@
-#input age and check if it is valid
-#if not, print reminding message
-#if so, input weight and check if it is valid
-#the same checking method as the age for weight
-#same for creatine concentration
-#input gender and check if it is valid
-#calculate CrCl for male and female with different formular
+#let user input all the variables and store them
+#check if the variables are all valid, if not, print the reminding message
+#calculate CrCl for male and female with different formular and print that out
 
+#store all the variables
 a=int(input('How old are you:')) #store age
-#check if age is valid
-if a>=100 or a<0: 
-    print("Please input valid age:")
+w=float(input('Please input your weight in kg:')) #store weight
+c=float(input("Please input your creatine concentration in μmol/l:")) #store creatine concentration
+g=str(input("Please input your gender(male or female):")) #store gender
+
+#check if all the variables are valid
+if a>=100 or a<0:  #for age
+    print("The age should be within 0~100!")
+if w<=20 or w>=80: #for weight
+    print("The weight should be within 20~80 kg!")
+if c<=0 or c>=100: #for creatine concentration
+    print("The creatine concentration should be within 0~100 μmol/l.") 
+#check if gender is valid, and if so, give the CrCl value by gender
+if g=='male':
+    CrCl=(140-a)*w/(72*c)
+    print("Your CrCl is",CrCl)
+elif g=='female':
+    CrCl=(140-a)*w*0.85/(72*c) #female's CrCl needs to multiply 0.85
+    print("Your CrCl is",CrCl)
 else:
-    w=float(input('Please input your weight in kg:')) #store weight
-    #check if weight is valid
-    if w<=20 or w>=80:
-        print("Please input valid weight.")
-    else:
-        c=float(input("Please input your creatine concentration in μmol/l:")) #store creatine concentration
-        #check if creatine concentration is valid
-        if c<=0 or c>=100:
-            print("Please input valid creatine concentration.") 
-        else:
-            g=str(input("Please input your gender(male or female):")) #store gender
-            #check if gender is valid, and if so, give the CrCl value by gender
-            if g=='male':
-                CrCl=(140-a)*w/(72*c)
-                print("Your CrCl is",CrCl)
-            elif g=='female':
-                CrCl=(140-a)*w*0.85/(72*c) #female's CrCl needs to multiply 0.85
-                print("Your CrCl is",CrCl)
-            else:
-                print("Please input a valid gender.")
+    print("Please input a valid gender.")
