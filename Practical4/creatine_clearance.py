cd6b8318389e6af2165f485c3bbcd3481ec60aef
apple <apple@appledeMacBook-Pro.local>
2026-03-13 10:36:14 +0800

    update some changes in creatine_clearance.py

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -1,3 +1,11 @@
+#input age and check if it is valid
+#if not, print reminding message
+#if so, input weight and check if it is valid
+#the same checking method as the age for weight
+#same for creatine concentration
+#input gender and check if it is valid
+#calculate CrCl for male and female with different formular
+
 a=int(input('How old are you:')) #store age
 #check if age is valid
 if a>=100 or a<0: 
@@ -14,12 +22,12 @@ else:
             print("Please input valid creatine concentration.") 
         else:
             g=str(input("Please input your gender(male or female):")) #store gender
-            #check if gender is valid, and if so, give the CrCl value 
+            #check if gender is valid, and if so, give the CrCl value by gender
             if g=='male':
                 CrCl=(140-a)*w/(72*c)
                 print("Your CrCl is",CrCl)
             elif g=='female':
-                CrCl=(140-a)*w*0.85/(72*c)
+                CrCl=(140-a)*w*0.85/(72*c) #female's CrCl needs to multiply 0.85
                 print("Your CrCl is",CrCl)
             else:
                 print("Please input a valid gender.")
