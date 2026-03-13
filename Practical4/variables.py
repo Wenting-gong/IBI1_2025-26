ae797d2e5db09b7513fb0ff1038f2ff0fd3b2e5b
apple <apple@appledeMacBook-Pro.local>
2026-03-13 11:27:50 +0800

    new changes to variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,13 +1,14 @@
-#store the variables
+#store the populations as variables
 a=5.08 #the population in 2004 is 5.08 million
 b=5.33 #the population in 2014 is 5.33 million
 c=5.55 #the population in 2024 is 5.55 million
-#calculate the growing population between 2004 and 2014
-d=b-a
+
+#calculate the growing population
+d=b-a #calculate the growing population between 2004 and 2014
 print('d=',d)
-#calculate the growing population between 2014 and 2024
-e=c-b
+e=c-b #calculate the growing population between 2014 and 2024
 print('e=',e)
+
 #compare the differences
 if d>e:
     print('The population growth decelerates.')
