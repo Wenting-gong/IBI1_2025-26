c0c97def3e6aaffbe57ed0a21d8d2c9e12571036
apple <apple@appledeMacBook-Pro.local>
2026-05-11 23:41:29 +0800

    Find all the ORF and write all the stop codons

--- Practical7/stop_codons.py
+++ Practical7/stop_codons.py
@@ -1,17 +1,17 @@
 import re
-#file name
+
 input_fa = "Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
 output_fa = "stop_genes.fa"
 
-# Read file
-f = open(input_fa, "r")
-lines = f.readlines()
-f.close()
+# Read fasta file
+with open(input_fa, "r") as f:
+    lines = f.readlines()
 
-#store genes in dictionary
+# Store gene names and sequences
 genes = {}
 current_name = ""
 current_seq = ""
+
 for line in lines:
     line = line.strip()
     if not line:
@@ -26,28 +26,29 @@ for line in lines:
 if current_name:
     genes[current_name] = current_seq
 
-#find stop codons
-out = open(output_fa, "w")
-start = "ATG"
-stops = {"TAA", "TAG", "TGA"}
-for name, seq in genes.items():
-    # find start codon
-    start_pos = seq.find(start)
-    if start_pos == -1:
-        continue
-    
-    # check for stop codons
-    found = set()
-    for i in range(start_pos + 3, len(seq) - 2, 3):
-        codon = seq[i:i+3]
-        if codon in stops:
-            found.add(codon)
-    
-    # write new file
-    if found:
-        stop_str = ",".join(sorted(found))
-        out.write(f">{name} {stop_str}\n")
-        out.write(seq + "\n")
+# Search for ORFs and stop codons
+with open(output_fa, "w") as out:
+    start_codon = "ATG"
+    stop_codons = {"TAA", "TAG", "TGA"}
 
-out.close()
+    for name, seq in genes.items():
+        found_stops = set()
+        # Find all positions of ATG in the sequence
+        all_atg_positions = [match.start() for match in re.finditer("ATG", seq)]
+
+        # Check every ORF starting from each ATG
+        for start_pos in all_atg_positions:
+            # Search in-frame codons after current ATG
+            for i in range(start_pos + 3, len(seq) - 2, 3):
+                codon = seq[i:i+3]
+                if codon in stop_codons:
+                    found_stops.add(codon)
+                    # Stop at the first stop codon of this ORF
+                    break
+
+        if found_stops:
+            stop_str = ",".join(sorted(found_stops))
+            out.write(f">{name} {stop_str}\n")
+            out.write(seq + "\n")
+
 print("Done!")
